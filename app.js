c55bcbc51e8956ddce5df1ea251ff5a70017d7d6
Revenage <romanvolobuev93@gmail.com>
2015-08-28 19:01:50 +0300

    add Graphic Donut

--- app.js
+++ app.js
@@ -118,7 +118,171 @@ if (Meteor.isClient) {
 
             $urlRouterProvider.otherwise("/accounts");
         }]);
+
+    angular.module('accoRun').controller('donutChartCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+
+        $scope.type = 'income';
+        $scope.accountsSort = $meteor.collection(function () {
+            return Accounts.find({type : $scope.type})
+        });
+
+        $scope.$watchCollection('accounts + type', function (newValue, oldValue) {
+            if (newValue) {
+            console.log('income accounts ' + $scope.accountsSort.length);
+                $scope.sumAccounts = totalSum();
+                change(initData());
+            }
+        });
+
+        function totalSum() {
+            var sum = 0;
+            for (var i = 0; i < $scope.accountsSort.length; i++) {
+                sum += $scope.accountsSort[i].cash;
+            }
+            return sum;
+        }
+
+        function initData (){
+            return $scope.accountsSort.map(function(d){
+                return {label: d.name + ' (' + (( d.cash / $scope.sumAccounts ) * 100).toFixed(2) +'%)' , value: d.cash}
+            });
+        }
+
+        function pastelColors(){
+            var r = (Math.round(Math.random()* 127) + 100).toString(16);
+            var g = (Math.round(Math.random()* 127) + 100).toString(16);
+            var b = (Math.round(Math.random()* 127) + 100).toString(16);
+            return '#' + r + g + b;
+        }
+
+        var svg = d3.select("div.donutChart")
+            .append("svg")
+            .append("g");
+
+        svg.append("g")
+            .attr("class", "slices");
+        svg.append("g")
+            .attr("class", "labels");
+        svg.append("g")
+            .attr("class", "lines");
+
+        var width = 960,
+            height = 450,
+            radius = Math.min(width, height) / 2;
+
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function(d) {
+                return d.value;
+            });
+
+        var arc = d3.svg.arc()
+            .outerRadius(radius * 0.8)
+            .innerRadius(radius * 0.4);
+
+        var outerArc = d3.svg.arc()
+            .innerRadius(radius * 0.9)
+            .outerRadius(radius * 0.9);
+
+        svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        var key = function(d){ return d.data.label; };
+
+        function change(data) {
+
+            /* ------- PIE SLICES -------*/
+            var slice = svg.select(".slices").selectAll("path.slice")
+                .data(pie(data), key);
+
+            slice.enter()
+                .insert("path")
+                .style("fill", function(d) { return pastelColors(); })
+                .attr("class", "slice");
+
+            slice
+                .transition().duration(1000)
+                .attrTween("d", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        return arc(interpolate(t));
+                    };
+                });
+
+            slice.exit()
+                .remove();
+
+            /* ------- TEXT LABELS -------*/
+
+            var text = svg.select(".labels").selectAll("text")
+                .data(pie(data), key);
+
+            text.enter()
+                .append("text")
+                .attr("dy", ".35em")
+                .text(function(d) {
+                    return d.data.label;
+                });
+
+            function midAngle(d){
+                return d.startAngle + (d.endAngle - d.startAngle)/2;
+            }
+
+            text.transition().duration(1000)
+                .attrTween("transform", function(d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        var pos = outerArc.centroid(d2);
+                        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+                        return "translate("+ pos +")";
+                    };
+                })
+                .styleTween("text-anchor", function(d){
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        return midAngle(d2) < Math.PI ? "start":"end";
+                    };
+                });
+
+            text.exit()
+                .remove();
+
+            /* ------- SLICE TO TEXT POLYLINES -------*/
+
+            var polyline = svg.select(".lines").selectAll("polyline")
+                .data(pie(data), key);
+
+            polyline.enter()
+                .append("polyline");
+
+            polyline.transition().duration(1000)
+                .attrTween("points", function(d){
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function(t) {
+                        var d2 = interpolate(t);
+                        var pos = outerArc.centroid(d2);
+                        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                        return [arc.centroid(d2), outerArc.centroid(d2), pos];
+                    };
+                });
+
+            polyline.exit()
+                .remove();
+        }
+
+    }]);
+
 }
+
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
@@ -126,15 +290,18 @@ if (Meteor.isServer) {
             var accounts = [
                 {
                     'name': 'Dubstep-Free Zone',
-                    'description': 'Fast just got faster with Nexus S.'
+                    'description': 'Fast just got faster with Nexus S.',
+                    'cost': 666
                 },
                 {
                     'name': 'All dubstep all the time',
-                    'description': 'Get it on!'
+                    'description': 'Get it on!',
+                    'cost': 666
                 },
                 {
                     'name': 'Savage lounging',
-                    'description': 'Leisure suit required. And only fiercest manners.'
+                    'description': 'Leisure suit required. And only fiercest manners.',
+                    'cost': 666
                 }
             ];
             for (var i = 0; i < accounts.length; i++)
