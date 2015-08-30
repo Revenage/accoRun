038b5b63fa017fcdfa38466dc76a1d0df427bb46
Revenage <romanvolobuev93@gmail.com>
2015-08-30 13:37:38 +0300

    make graphic relative width, add behavoir? then graphic sector under 3% not visibled

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (Meteor.isClient) {
             //$cookies.put("sortReverse", $scope.sortReverse);
         };
 
-        $scope.addAccountsRow = function (name, cash) {
+        $scope.addAccountsRow = function (name, date, cash) {
 
             $scope.cashError = false;
             $scope.nameError = false;
@@ -51,7 +51,7 @@ if (Meteor.isClient) {
                 $scope.newAcco.type = 'zero';
             }
             $scope.inputRow = false;
-            $scope.newAcco.date = new Date();
+            $scope.newAcco.date = date;
             $scope.newAcco.nowDate = new Date();
 
             $scope.accounts.push($scope.newAcco);
@@ -120,21 +120,34 @@ if (Meteor.isClient) {
         }]);
 
 
-    angular.module('accoRun').directive('donutGraphic', ['$meteor', function ($meteor) {
+    angular.module('accoRun').directive('donutGraphic', ['$meteor','$window', function ($meteor, $window) {
         return function (scope, element, attrs) {
+            var graphWidth = element[0].offsetWidth;
 
-            console.log(element[0].offsetHeight);
-            console.log(element[0].offsetWidth);
-            $(element).css('width', attrs.width + "px")
-                      .css('height', attrs.height + "px");
+            function setFontSize() {
+                if (graphWidth < 700) {
+                   return graphWidth / 50;
+                } else {
+                    return 14;
+                }
+            }
+
+            $(element).css('width', graphWidth + "px")
+                      .css('height', graphWidth / 2 + "px")
+                      .css("font-size", setFontSize() +"px");
+
             scope.type = attrs.sorttype;
             scope.accountsSort = $meteor.collection(function () {
                 return Accounts.find({type : scope.type})
             });
 
+/*            angular.element($window).bind('resize', function() {
+                change(initData());
+                scope.$apply();
+            });*/
+
             scope.$watchCollection('accounts + type', function (newValue, oldValue) {
                 if (newValue) {
-                    console.log('income accounts ' + scope.accountsSort.length);
                     scope.sumAccounts = totalSum();
                     change(initData());
                 }
@@ -172,8 +185,8 @@ if (Meteor.isClient) {
             svg.append("g")
                 .attr("class", "lines");
 
-            var width = attrs.width,
-                height = attrs.height,
+            var width = graphWidth,
+                height = graphWidth / 2,
                 radius = Math.min(width, height) / 2;
 
             var pie = d3.layout.pie()
@@ -228,7 +241,9 @@ if (Meteor.isClient) {
                     .append("text")
                     .attr("dy", ".35em")
                     .text(function(d) {
-                        return d.data.label;
+                        if ((( d.value / scope.sumAccounts ) * 100).toFixed(2) > 3) {
+                            return d.data.label;
+                        }
                     });
 
                 function midAngle(d){
@@ -266,19 +281,31 @@ if (Meteor.isClient) {
                     .data(pie(data), key);
 
                 polyline.enter()
-                    .append("polyline");
+                .append("polyline");
+
+                /*text.enter()
+                 .append("text")
+                 .attr("dy", ".35em")
+                 .text(function(d) {
+
+                 return d.data.label;
+                 }
+                 });*/
+
 
                 polyline.transition().duration(1000)
                     .attrTween("points", function(d){
                         this._current = this._current || d;
                         var interpolate = d3.interpolate(this._current, d);
                         this._current = interpolate(0);
-                        return function(t) {
-                            var d2 = interpolate(t);
-                            var pos = outerArc.centroid(d2);
-                            pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-                            return [arc.centroid(d2), outerArc.centroid(d2), pos];
-                        };
+                        if ((( d.value / scope.sumAccounts ) * 100).toFixed(2) > 3) {
+                            return function(t) {
+                             var d2 = interpolate(t);
+                             var pos = outerArc.centroid(d2);
+                             pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                             return [arc.centroid(d2), outerArc.centroid(d2), pos];
+                             };
+                        }
                     });
 
                 polyline.exit()
@@ -287,169 +314,6 @@ if (Meteor.isClient) {
 
         }
     }]);
-    
-    /*angular.module('accoRun').controller('donutChartCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
-
-        $scope.type = 'income';
-        $scope.accountsSort = $meteor.collection(function () {
-            return Accounts.find({type : $scope.type})
-        });
-
-        $scope.$watchCollection('accounts + type', function (newValue, oldValue) {
-            if (newValue) {
-            console.log('income accounts ' + $scope.accountsSort.length);
-                $scope.sumAccounts = totalSum();
-                change(initData());
-            }
-        });
-
-        function totalSum() {
-            var sum = 0;
-            for (var i = 0; i < $scope.accountsSort.length; i++) {
-                sum += $scope.accountsSort[i].cash;
-            }
-            return sum;
-        }
-
-        function initData (){
-            return $scope.accountsSort.map(function(d){
-                return {label: d.name + ' (' + (( d.cash / $scope.sumAccounts ) * 100).toFixed(2) +'%)' , value: d.cash}
-            });
-        }
-
-        function pastelColors(){
-            var r = (Math.round(Math.random()* 127) + 100).toString(16);
-            var g = (Math.round(Math.random()* 127) + 100).toString(16);
-            var b = (Math.round(Math.random()* 127) + 100).toString(16);
-            return '#' + r + g + b;
-        }
-
-        var svg = d3.select("div.donutChart")
-            .append("svg")
-            .append("g");
-
-        svg.append("g")
-            .attr("class", "slices");
-        svg.append("g")
-            .attr("class", "labels");
-        svg.append("g")
-            .attr("class", "lines");
-
-        var width = 600,
-            height = 400,
-            radius = Math.min(width, height) / 2;
-
-        var pie = d3.layout.pie()
-            .sort(null)
-            .value(function(d) {
-                return d.value;
-            });
-
-        var arc = d3.svg.arc()
-            .outerRadius(radius * 0.8)
-            .innerRadius(radius * 0.4);
-
-        var outerArc = d3.svg.arc()
-            .innerRadius(radius * 0.9)
-            .outerRadius(radius * 0.9);
-
-        svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        var key = function(d){ return d.data.label; };
-
-        function change(data) {
-
-            /!* ------- PIE SLICES -------*!/
-            var slice = svg.select(".slices").selectAll("path.slice")
-                .data(pie(data), key);
-
-            slice.enter()
-                .insert("path")
-                .style("fill", function(d) { return pastelColors(); })
-                .attr("class", "slice");
-
-            slice
-                .transition().duration(1000)
-                .attrTween("d", function(d) {
-                    this._current = this._current || d;
-                    var interpolate = d3.interpolate(this._current, d);
-                    this._current = interpolate(0);
-                    return function(t) {
-                        return arc(interpolate(t));
-                    };
-                });
-
-            slice.exit()
-                .remove();
-
-            /!* ------- TEXT LABELS -------*!/
-
-            var text = svg.select(".labels").selectAll("text")
-                .data(pie(data), key);
-
-            text.enter()
-                .append("text")
-                .attr("dy", ".35em")
-                .text(function(d) {
-                    return d.data.label;
-                });
-
-            function midAngle(d){
-                return d.startAngle + (d.endAngle - d.startAngle)/2;
-            }
-
-            text.transition().duration(1000)
-                .attrTween("transform", function(d) {
-                    this._current = this._current || d;
-                    var interpolate = d3.interpolate(this._current, d);
-                    this._current = interpolate(0);
-                    return function(t) {
-                        var d2 = interpolate(t);
-                        var pos = outerArc.centroid(d2);
-                        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-                        return "translate("+ pos +")";
-                    };
-                })
-                .styleTween("text-anchor", function(d){
-                    this._current = this._current || d;
-                    var interpolate = d3.interpolate(this._current, d);
-                    this._current = interpolate(0);
-                    return function(t) {
-                        var d2 = interpolate(t);
-                        return midAngle(d2) < Math.PI ? "start":"end";
-                    };
-                });
-
-            text.exit()
-                .remove();
-
-            /!* ------- SLICE TO TEXT POLYLINES -------*!/
-
-            var polyline = svg.select(".lines").selectAll("polyline")
-                .data(pie(data), key);
-
-            polyline.enter()
-                .append("polyline");
-
-            polyline.transition().duration(1000)
-                .attrTween("points", function(d){
-                    this._current = this._current || d;
-                    var interpolate = d3.interpolate(this._current, d);
-                    this._current = interpolate(0);
-                    return function(t) {
-                        var d2 = interpolate(t);
-                        var pos = outerArc.centroid(d2);
-                        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-                        return [arc.centroid(d2), outerArc.centroid(d2), pos];
-                    };
-                });
-
-            polyline.exit()
-                .remove();
-        }
-
-    }]);*/
-
 }
 
 
